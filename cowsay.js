aec161580d1773740ce6f945c944ea8e3afa5973
CSJ <tmdwns9630@gmail.com>
2022-09-14 18:41:13 +0900

    8일차 끝

--- cowsay.js
+++ cowsay.js
@@ -1,14 +1,15 @@
-// //console.log('hello nodejs');
-
-// // or cowsay.think()
-
-const cowsay = require('cowsay');
+// or cowsay.think()
+const cowsay = require("cowsay");
 // 라이브러리를 require로 가져온다.
+//일종의 #include < >로 보면 된다.
 
 console.log(
   cowsay.say({
-    text: '역배들, 정신이 들어?',
-    e: '^ ^',
-    T: 'ㅠ ',
-  }),
+    text: "역배들, 정신이 들어?",
+    e: "^ ^",
+    T: "ㅠ ",
+  })
 );
+
+require("dotenv").config();
+console.log(process.env.SECRET_KEY); // remove this after you've confirmed it working
